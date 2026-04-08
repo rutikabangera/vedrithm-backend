f78f2877a9cf9042943f1c537cade49cd8122cd3
2744 <rutika.bangera@mindcraft.co.in>
2026-04-08 20:43:11 +0530

    CORS

--- src/main/java/com/vedrithm/config/CorsConfig.java
+++ src/main/java/com/vedrithm/config/CorsConfig.java
@@ -2,19 +2,25 @@ package com.vedrithm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
+                        .allowedOrigins(
+                                "https://vedrithm.pages.dev",
+                                "http://localhost:4200"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
